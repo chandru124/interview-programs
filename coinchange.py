b9b50940c768af7872e7a62f0148a598bc421a94
chandru124 <87891238+chandru124@users.noreply.github.com>
2021-11-26 17:11:55 +0530

    Create coinchange.py

---
+++ coinchange.py
@@ -0,0 +1,22 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int: 
+         
+        if amount == 0 :
+            return 0
+        if min(coins) > amount:
+            return -1
+        dp = [-1 for i in range(0, amount + 1)]
+        for i in coins:
+            if i > len(dp) - 1:
+                continue
+            dp[i] = 1
+            for j in range (i + 1, amount + 1):
+                if dp[j -i] == -1:
+                    continue 
+                elif dp[j] == -1:
+                    dp[j] = dp[j - i] + 1
+                else:
+                    dp[j] = min(dp[j], dp[j - i] + 1)
+         
+        return dp[amount] 
+        
